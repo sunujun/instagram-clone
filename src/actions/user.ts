ceea7fe5f3c65c49932d3a8213136d8f7249e829
seongjun.oh <tiger8708@gmail.com>
2023-03-11 02:20:09 +0900

    getMyFeedList 추가

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -47,6 +47,48 @@ export const signIn = (): IUserInfoThunkAction => async dispatch => {
     );
 };
 
+export const getMyFeedList = (): IUserInfoThunkAction => async dispatch => {
+    dispatch(getMyFeedRequest());
+    await sleep(500);
+    dispatch(
+        getMyFeedSuccess([
+            {
+                id: 'ID_01',
+                content: 'CONTENT_01',
+                writer: {
+                    name: 'WRITER_NAME_01',
+                    uid: 'WRITER_UID_01',
+                },
+                imageUrl: 'https://docs.expo.dev/static/images/tutorial/background-image.png',
+                likeHistory: ['UID_01', 'UID_02', 'UID_03'],
+                createdAt: new Date().getTime(),
+            },
+            {
+                id: 'ID_02',
+                content: 'CONTENT_02',
+                writer: {
+                    name: 'WRITER_NAME_02',
+                    uid: 'WRITER_UID_02',
+                },
+                imageUrl: 'https://docs.expo.dev/static/images/tutorial/background-image.png',
+                likeHistory: ['UID_01', 'UID_02', 'UID_03'],
+                createdAt: new Date().getTime(),
+            },
+            {
+                id: 'ID_03',
+                content: 'CONTENT_03',
+                writer: {
+                    name: 'WRITER_NAME_03',
+                    uid: 'WRITER_UID_03',
+                },
+                imageUrl: 'https://docs.expo.dev/static/images/tutorial/background-image.png',
+                likeHistory: ['UID_01', 'UID_02', 'UID_03'],
+                createdAt: new Date().getTime(),
+            },
+        ]),
+    );
+};
+
 export type IUserInfoDispatch = ThunkDispatch<RootState, undefined, IUserInfoActions>;
 export type IUserInfoThunkAction = ThunkAction<void, RootState, undefined, IUserInfoActions>;
 export type IUserInfoActions =
