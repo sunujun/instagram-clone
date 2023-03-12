da2c5bd7558c9e21ee664a4a7b862003b07680ff
seongjun.oh <tiger8708@gmail.com>
2023-03-12 11:57:40 +0900

    google sign in configure

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { RootApp } from './src/RootApp';
 import store from './src/store/store';
 
 function App(): JSX.Element {
+    const googleSignInConfigure = () => {
+        GoogleSignin.configure({
+            webClientId: '135091833054-l84ja05356etijq9hke6poqpqlarih4p.apps.googleusercontent.com',
+        });
+    };
+
+    useEffect(() => {
+        googleSignInConfigure();
+    }, []);
+
     return (
         <Provider store={store}>
             <SafeAreaProvider>
